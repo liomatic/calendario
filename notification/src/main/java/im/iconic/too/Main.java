1d3dac679b7f2128efb7ecdd96a436fae3e35764
Vincenzino Lio <vincenzino.lio@external.nexigroup.com>
2024-12-13 11:06:37 +0100

    Configurazione datasource H2 per il progetto catalog

--- notification/src/main/java/im/iconic/too/Main.java
+++ notification/src/main/java/im/iconic/too/Main.java
@@ -1,4 +1,4 @@
-package it.iti.monaco;
+package im.iconic.too;
 
 public class Main {
     public static void main(String[] args) {
--- web-app/src/main/java/it/iti/monaco/Main.java
+++
@@ -1,7 +0,0 @@
-package it.iti.monaco;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
