0df536f54502fe056b7f7054a8953bba1a7cca50
pace.andrea <s.andrea.pace@itimonaco.it>
2025-01-24 13:35:25 +0100

    ricerca per marca e unità disponibili

--- catalog/src/main/java/im/iconic/too/repository/CatalogRepository.java
+++ catalog/src/main/java/im/iconic/too/repository/CatalogRepository.java
@@ -3,6 +3,7 @@ package im.iconic.too.repository;
 import im.iconic.too.model.Catalog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,11 @@ public interface CatalogRepository
 
     @Query("SELECT c FROM Catalog c WHERE c.unitaDisponibili = 0")
     List<Catalog> soldOut();
+
+    @Query("SELECT c FROM Catalog c WHERE c.unitaDisponibili <= 10")
+    List<Catalog> findLowAvailability(@Param("soglia") int soglia);
+
+    @Query("SELECT c FROM Catalog c WHERE c.marca = :marca")
+    List<Catalog> findCompanies(@Param("marca") String marca);
 
 }
