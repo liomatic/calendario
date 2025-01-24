ae46881beba4f2a02b6b7647dcc6542579aed018
p.cilio <pierdomenico.cilio@gmail.com>
2025-01-24 13:32:30 +0100

    nuovi metodi per la select di cataloghi

--- catalog/src/main/resources/data.sql
+++ catalog/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO catalog (id, name, marca, modello,
-                     unita_disponibili, unita_contenute, codice)
+                     unita_disponibili, unita_contenute, codice, tipologia)
     VALUES (1, 'Notebook', 'DELL',
-            'Latitude 550', 100, 1, 'NDL550');
+            'Latitude 550', 100, 1, 'NDL550', 'computer');
 
 INSERT INTO catalog (id, name, marca, modello,
-                     unita_disponibili, unita_contenute, codice)
+                     unita_disponibili, unita_contenute, codice, tipologia)
 VALUES (2, 'Carrello', 'Marca del carrello',
-        'Carrello 550', 10, 10, 'CAR550');
+        'Carrello 550', 10, 10, 'CAR550', 'carrello');
