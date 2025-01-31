fba8f17c7f1ada9830867279cfced59b10cb604f
Giovanni <s.giovanni.scarpino@itimonaco.it>
2025-01-31 12:10:48 +0100

    dipendenze calendar

---
+++ calendar/src/main/java/im/iconic/too/CalendarApplication.java
@@ -0,0 +1,27 @@
+package im.iconic.too;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableTransactionManagement
+@EnableJpaRepositories
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableCaching
+@EnableWebMvc
+@EnableScheduling
+public class CalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarApplication.class, args);
+    }
+
+}
