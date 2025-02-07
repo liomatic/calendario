6e0c87e481567151591935f00725c34b8302a035
luigifonzies <danielcasciaro01@gmail.com>
2025-02-07 11:42:48 +0100

    Entity e DTO

--- calendar/src/main/java/im/iconic/too/model/Reservation.java
+++ calendar/src/main/java/im/iconic/too/model/Reservation.java
@@ -1,9 +1,6 @@
 package im.iconic.too.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
+import jakarta.persistence.*;
 
 import java.util.Date;
 
